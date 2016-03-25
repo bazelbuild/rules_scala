05cd92a5648d4b49e1d18722050f2382bffabd46
Oscar Boykin <oscar@stripe.com>
2016-03-24 15:15:25 -1000

    Add a bazel xml test formatter

--- test_run.sh
+++ test_run.sh
@@ -6,4 +6,6 @@ bazel build test/... \
   && bazel run test:ScalaBinary \
   && bazel run test:ScalaLibBinary \
   && bazel run test:JavaBinary \
-  && bazel test test/...
+  && bazel test test/... \
+  && (find -L ./bazel-testlogs -iname "*.scala" | xargs -n1 xmllint > /dev/null) \
+  && echo "all good"
