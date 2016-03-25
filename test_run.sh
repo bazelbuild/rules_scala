908f9a731d69482bb075680b754d15fba31c93eb
Oscar Boykin <oscar@stripe.com>
2016-03-24 15:24:37 -1000

    search for xml

--- test_run.sh
+++ test_run.sh
@@ -7,5 +7,6 @@ bazel build test/... \
   && bazel run test:ScalaLibBinary \
   && bazel run test:JavaBinary \
   && bazel test test/... \
-  && (find -L ./bazel-testlogs -iname "*.scala" | xargs -n1 xmllint > /dev/null) \
+  && find -L ./bazel-testlogs -iname "*.xml" \
+  && (find -L ./bazel-testlogs -iname "*.xml" | xargs -n1 xmllint > /dev/null) \
   && echo "all good"
