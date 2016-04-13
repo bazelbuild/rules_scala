8f8b89b98f4811dfcef7e0758bb04b193e03df90
Jonathan Coveney <jco@stripe.com>
2016-04-13 17:35:43 -0400

    Add test that shows failure of current exports handling

---
+++ test/A.scala
@@ -0,0 +1,7 @@
+package scala.test
+
+object A {
+  def main(args: Array[String]) {
+    println("4 8 15 16 23 42")
+  }
+}
---
+++ test/D.scala
@@ -0,0 +1,7 @@
+package scala.test
+
+object D {
+  def main(args: Array[String]) {
+    A.main(args)
+  }
+}
