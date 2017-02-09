fd6aa1ea909487020c59a48912c829d42e6e0dad
P. Oscar Boykin <johnynek@users.noreply.github.com>
2017-02-09 10:41:18 -1000

    Avoid decompressing jars/zips in scrooge support (#137)
    * Avoid decompressing jars/zips in scrooge support
    
    * Use a custom importer to deal with zips
    
    * Fix paths with leading .. in thrift includes
    
    * Add a test case for ./ includes
    
    * Reuse the ZipFile in FocusedZipImporter
    
    * address Ian's comments

---
+++ src/scala/io/bazel/rules_scala/scrooge_support/Compiler.scala
@@ -0,0 +1,162 @@
+package io.bazel.rules_scala.scrooge_support
+/*
+ * Copyright 2011 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * copy-pasta from:
+ * https://github.com/twitter/scrooge/blob/develop/scrooge-generator/src/main/scala/com/twitter/scrooge/Compiler.scala
+ * to customize the Importers
+ *
+ * Since scrooge always adds a Importer(".") it would find it from that path by happenstance of how bazel works, but
+ * not if there is sandboxing preventing it from seeing it.
+ *
+ * The solution is make a new Compiler class, don't reuse thrifts, and have better control of how importing works.
+ * Doing this I learned that scrooge's zip/jar support is pretty broken and really only works if all imports are
+ * considered relative to some fictional root directory (a style done at Twitter, but not really standard for thrift).
+ * With my new Compiler, I could only add the importers I want. I made a new importer which is a zip importer that also
+ * knows where it is in the zip file. In this way, it could use a thrift jar that itself has internal relative imports
+ * (such as our monster thrift jar at Stripe).
+ */
+
+import com.twitter.scrooge._
+import com.twitter.scrooge.ast.Document
+import com.twitter.scrooge.backend.{GeneratorFactory, ScalaGenerator, ServiceOption}
+import com.twitter.scrooge.frontend.{FileParseException, TypeResolver, ThriftParser, Importer, MultiImporter, ZipImporter}
+import java.io.{File, FileWriter}
+import java.nio.file.Paths
+import java.util.jar.{ JarFile, JarEntry }
+import scala.collection.concurrent.TrieMap
+import scala.collection.mutable
+
+object CompilerDefaults {
+  var language: String = "scala"
+  var defaultNamespace: String = "thrift"
+
+  def listJar(_jar: File): List[String] = {
+    val files = List.newBuilder[String]
+    val jar = new JarFile(_jar)
+    val enumEntries = jar.entries()
+    while (enumEntries.hasMoreElements) {
+      val file = enumEntries.nextElement().asInstanceOf[JarEntry]
+      if (!file.isDirectory) {
+        files += file.getName
+      }
+    }
+    files.result()
+  }
+}
+
+class Compiler {
+  val defaultDestFolder = "."
+  var destFolder: String = defaultDestFolder
+  // These are jars we are including, but are not compiling
+  val includeJars = new mutable.ListBuffer[String]
+  // these are the jars we want to compile into scala source jars
+  val compileJars = new mutable.ListBuffer[String]
+  val flags = new mutable.HashSet[ServiceOption]
+  val namespaceMappings = new mutable.HashMap[String, String]
+  var verbose = false
+  var strict = true
+  var skipUnchanged = false
+  var experimentFlags = new mutable.ListBuffer[String]
+  var fileMapPath: scala.Option[String] = None
+  var fileMapWriter: scala.Option[FileWriter] = None
+  var dryRun: Boolean = false
+  var language: String = CompilerDefaults.language
+  var defaultNamespace: String = CompilerDefaults.defaultNamespace
+  var scalaWarnOnJavaNSFallback: Boolean = false
+
+
+  def run() {
+    // if --gen-file-map is specified, prepare the map file.
+    fileMapWriter = fileMapPath.map { path =>
+      val file = new File(path)
+      val dir = file.getParentFile
+      if (dir != null && !dir.exists()) {
+        dir.mkdirs()
+      }
+      if (verbose) {
+        println("+ Writing file mapping to %s".format(path))
+      }
+      new FileWriter(file)
+    }
+
+    val allJars: List[File] =
+      ((includeJars.toList) ::: (compileJars.toList))
+        .map { path => (new File(path)).getCanonicalFile }
+
+    val isJava = language.equals("java")
+    val documentCache = new TrieMap[String, Document]
+
+    // compile
+    val allPaths = for {
+      jar <- compileJars.iterator
+      inputFullPath <- CompilerDefaults.listJar(new File(jar)).iterator
+    } yield inputFullPath
+
+    val rootImporter = FocusedZipImporter.forPaths(None, allJars)
+    allPaths.foreach { inputFullPath =>
+      try {
+        val inputFile = Paths.get(inputFullPath).getFileName.toString
+        val focus = Option((new File(inputFullPath)).getParentFile)
+        // allow lookup either focused, or relative to the root of the repo
+        val importer = rootImporter.copy(focus = focus) +: rootImporter
+        val parser = new ThriftParser(
+          importer,
+          strict,
+          defaultOptional = isJava,
+          skipIncludes = false,
+          documentCache
+        )(com.twitter.logging.NullLogger) // scrooge warns on file names with "/"
+        val doc = parser.parseFile(inputFile).mapNamespaces(namespaceMappings.toMap)
+
+        if (verbose) println("+ Compiling %s".format(inputFile))
+        val resolvedDoc = TypeResolver()(doc)
+        val generator = GeneratorFactory(
+          language,
+          resolvedDoc,
+          defaultNamespace,
+          experimentFlags)
+
+        generator match {
+          case g: ScalaGenerator => g.warnOnJavaNamespaceFallback = scalaWarnOnJavaNSFallback
+          case _ => ()
+        }
+
+        val generatedFiles = generator(
+          flags.toSet,
+          new File(destFolder),
+          dryRun
+        ).map {
+          _.getPath
+        }
+        if (verbose) {
+          println("+ Generated %s".format(generatedFiles.mkString(", ")))
+        }
+        fileMapWriter.foreach { w =>
+          generatedFiles.foreach { path =>
+            w.write(inputFullPath + " -> " + path + "\n")
+          }
+        }
+      } catch {
+        case e: Throwable => throw new FileParseException(inputFullPath, e)
+      }
+    }
+
+    // flush and close the map file
+    fileMapWriter.foreach { _.close() }
+  }
+}
--- src/scala/scripts/TwitterScroogeGenerator.scala
+++ src/scala/scripts/TwitterScroogeGenerator.scala
@@ -1,54 +1,12 @@
 package scripts
 
-import com.twitter.scrooge.Compiler
-
+import io.bazel.rules_scala.scrooge_support.{ Compiler, CompilerDefaults }
+import io.bazel.rulesscala.jar.JarCreator
+import java.io.{ File, FileOutputStream, IOException }
+import java.nio.file.attribute.{ BasicFileAttributes, FileTime }
+import java.nio.file.{ Files, SimpleFileVisitor, FileVisitResult, Path, Paths }
 import scala.collection.mutable.Buffer
 import scala.io.Source
-
-import java.io.{ File, FileOutputStream, IOException }
-import java.nio.file.{ Files, SimpleFileVisitor, FileVisitResult, Path, Paths }
-import java.nio.file.attribute.{ BasicFileAttributes, FileTime }
-import java.util.jar.{ JarEntry, JarFile, JarOutputStream }
-
-object FinalJarCreator {
-  val gm = """(\S+) -> (\S+)""".r
-
-  def apply(dest: Path, owned: Set[Path], genFileMap: Path, scroogeDir: Path) {
-    val genmap = Source.fromFile(genFileMap.toString)
-      .getLines
-      .foldLeft(Map.empty[String, Set[String]]) { case (m, gm(thrift, gen)) =>
-        m.+((thrift, m.getOrElse(thrift, Set.empty[String]) + gen))
-      }
-    val shouldMove =
-      owned.map(_.toString).foldLeft(Set.empty[String]) { (s, n) =>
-        genmap.get(n).fold(s) { s ++ _ }
-      }.map { Paths.get(_).normalize }
-    val jar = new JarOutputStream(new FileOutputStream(dest.toFile))
-    Files.walkFileTree(
-      scroogeDir,
-      FinalJarCreator(scroogeDir, jar, shouldMove)
-    )
-    jar.close()
-  }
-}
-case class FinalJarCreator(_baseDir: Path, jar: JarOutputStream, shouldMove: Set[Path]) extends SimpleFileVisitor[Path] {
-  val baseDir = _baseDir.normalize
-
-  // We return the path of the file to add to the jar
-  def shouldVisitFile(file: Path): Option[Path] =
-    if (shouldMove.contains(file)) Some(baseDir.relativize(file))
-    else None
-
-  override def visitFile(file: Path, attr: BasicFileAttributes) = {
-    shouldVisitFile(file).foreach { _file =>
-      val entry = new JarEntry(_file.toString)
-      entry.setTime(198001010000L)
-      jar.putNextEntry(entry)
-      Files.copy(file, jar)
-    }
-    FileVisitResult.CONTINUE
-  }
-}
 
 object DeleteRecursively extends SimpleFileVisitor[Path] {
   override def visitFile(file: Path, attr: BasicFileAttributes) = {
@@ -63,13 +21,6 @@ object DeleteRecursively extends SimpleFileVisitor[Path] {
   }
 }
 
-case class ForeachFile(f: Path => Unit) extends SimpleFileVisitor[Path] {
-  override def visitFile(file: Path, attr: BasicFileAttributes) = {
-    f(file)
-    FileVisitResult.CONTINUE
-  }
-}
-
 object ScroogeGenerator {
   def deleteDir(path: Path) {
     try {
@@ -79,28 +30,9 @@ object ScroogeGenerator {
     }
   }
 
-  def extractJarTo(_jar: Path, _dest: Path): List[Path] = {
-    val files = Buffer[Path]()
-    val jar = new JarFile(_jar.toFile)
-    val enumEntries = jar.entries()
-    while (enumEntries.hasMoreElements) {
-      val file = enumEntries.nextElement().asInstanceOf[JarEntry]
-      val path = _dest.resolve(file.getName)
-      if (file.isDirectory) Files.createDirectories(path)
-      else {
-        val is = jar.getInputStream(file)
 
-        try Files.copy(is, path) // Will error out if path already exists
-        finally is.close()
-
-        files += path
-      }
-    }
-    files.toList
-  }
-
-  def readLinesAsPaths(path: Path): Set[Path] =
-    Source.fromFile(path.toString).getLines.map(Paths.get(_)).toSet
+  def readLines(path: Path): List[String] =
+    Source.fromFile(path.toString).getLines.toSet.toList.sorted
 
   def main(args: Array[String]) {
     if (args.length != 4) sys.error("Need to ensure enough arguments! " +
@@ -109,7 +41,7 @@ object ScroogeGenerator {
 
     val onlyTransitiveThriftSrcsFile = Paths.get(args(0))
     val immediateThriftSrcsFile = Paths.get(args(1))
-    val jarOutput = Paths.get(args(2))
+    val jarOutput = args(2)
     val remoteJarsFile = Paths.get(args(3))
 
     val tmp = Paths.get(Option(System.getenv("TMPDIR")).getOrElse("/tmp"))
@@ -117,53 +49,40 @@ object ScroogeGenerator {
 
     // These are all of the files to include when generating scrooge
     // Should not include anything in immediateThriftSrcs
-    val onlyTransitiveThriftSrcJars = readLinesAsPaths(onlyTransitiveThriftSrcsFile)
+    val onlyTransitiveThriftSrcJars = readLines(onlyTransitiveThriftSrcsFile)
 
     // These are the files whose output we want
-    val immediateThriftSrcJars = readLinesAsPaths(immediateThriftSrcsFile)
-
-    val genFileMap = scroogeOutput.resolve("gen-file-map.txt")
+    val immediateThriftSrcJars = readLines(immediateThriftSrcsFile)
+    // remote jars are jars that come from another repo, really no different from transitive
+    val remoteSrcJars = readLines(remoteJarsFile)
 
     val scrooge = new Compiler
 
-    // we need to extract into the same tree, as that is the only way to get relative imports between them working..
-    // AS SUCH, we are just going to try extracting EVERYTHING to the same tree, and we will just error if there
+    scrooge.compileJars ++= immediateThriftSrcJars
+    scrooge.includeJars ++= onlyTransitiveThriftSrcJars
+    scrooge.includeJars ++= remoteSrcJars
+
+    // should check that we have no overlap in any of the types
+    // we are just going to try extracting EVERYTHING to the same tree, and we will just error if there
     // are more than one.
-    val _tmp =  Files.createTempDirectory(tmp, "jar")
-    // This will only be meaningful if they have absolute_prefix set
-    scrooge.includePaths += _tmp.toString
+    def allFilesInZips(fs: List[String]): Set[String] =
+      fs.flatMap { f => CompilerDefaults.listJar(new File(f)) }.toSet
 
-    def extract(jars: Set[Path]): Set[Path] =
-      jars.flatMap { jar =>
-        val files = extractJarTo(jar, _tmp)
-        files.foreach { scrooge.includePaths += _.toString }
-        files
-      }
-
-    val immediateThriftSrcs = extract(immediateThriftSrcJars)
-
-    immediateThriftSrcs.foreach { scrooge.thriftFiles += _.toString }
-
-    val onlyTransitiveThriftSrcs = extract(onlyTransitiveThriftSrcJars)
-
-    val intersect = onlyTransitiveThriftSrcs.intersect(immediateThriftSrcs)
+    val immediateThriftSrcs = allFilesInZips(immediateThriftSrcJars)
+    val intersect = allFilesInZips(onlyTransitiveThriftSrcJars)
+      .intersect(immediateThriftSrcs)
 
     if (intersect.nonEmpty)
       sys.error("onlyTransitiveThriftSrcs and immediateThriftSrcs should " +
         s"have not intersection, found: ${intersect.mkString(",")}")
 
-    val remoteSrcJars = readLinesAsPaths(remoteJarsFile)
-    extract(remoteSrcJars)
-
-    val dirsToDelete = Set(scroogeOutput, _tmp)
-
+    val dirsToDelete = Set(scroogeOutput)
     scrooge.destFolder = scroogeOutput.toString
-    scrooge.fileMapPath = Some(genFileMap.toString)
     //TODO we should make this configurable
     scrooge.strict = false
     scrooge.run()
 
-    FinalJarCreator(jarOutput, immediateThriftSrcs, genFileMap, scroogeOutput)
+    JarCreator.buildJar(Array(jarOutput, scroogeOutput.toString))
 
     // Clean it out to be idempotent
     dirsToDelete.foreach { deleteDir(_) }
