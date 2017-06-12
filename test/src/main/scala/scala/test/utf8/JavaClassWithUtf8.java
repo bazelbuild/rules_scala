32a9a55b12ccc18ea50af9c31da7b5927421b4ee
Or Shachar <ors@wix.com>
2017-06-12 20:11:56 +0300

    reduced to minimal fields

--- test/src/main/scala/scala/test/utf8/JavaClassWithUtf8.java
+++ test/src/main/scala/scala/test/utf8/JavaClassWithUtf8.java
@@ -1,6 +1,5 @@
 package scala.test.utf8;
 
 class JavaClassWithUtf8{
-	public static final String CHINESES_STR = "http://www.example.com/真　12花柳芳/bla";
 	public static final String UTF_8_STR = "‡"; 
 }
--- test/src/main/scala/scala/test/utf8/ScalaClassWithUtf8.scala
+++ test/src/main/scala/scala/test/utf8/ScalaClassWithUtf8.scala
@@ -1,6 +1,5 @@
 package scala.test.utf8
 
 class ScalaClassWithUTF8{
-	val ChineseString:String = "http://www.example.com/真　12花柳芳/bla"
-	val RandomUtfString = "‡"
+	val RandomUtfString:String = "‡"
 }
