62f0959c422d8802b061a7f77c02c9c6a4c8a836
Or Shachar <ors@wix.com>
2017-06-12 20:01:29 +0300

    forcing utf-8 encoding with javac

---
+++ test/src/main/scala/scala/test/utf8/JavaClassWithUtf8.java
@@ -0,0 +1,6 @@
+package scala.test.utf8;
+
+class JavaClassWithUtf8{
+	public static final String CHINESES_STR = "http://www.example.com/真　12花柳芳/bla";
+	public static final String UTF_8_STR = "‡"; 
+}
---
+++ test/src/main/scala/scala/test/utf8/ScalaClassWithUtf8.scala
@@ -0,0 +1,6 @@
+package scala.test.utf8
+
+class ScalaClassWithUTF8{
+	val ChineseString:String = "http://www.example.com/真　12花柳芳/bla"
+	val RandomUtfString = "‡"
+}
