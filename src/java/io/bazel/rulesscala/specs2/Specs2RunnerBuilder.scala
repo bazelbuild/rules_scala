facea8ee01fe20337e65c52c1e9dbd429b3a2e30
Igal Tabachnik <igalt@wix.com>
2017-11-22 11:45:09 +0200

    Refactored specs2 description filtering a bit to clear up the intent

--- src/java/io/bazel/rulesscala/specs2/Specs2RunnerBuilder.scala
+++ src/java/io/bazel/rulesscala/specs2/Specs2RunnerBuilder.scala
@@ -3,7 +3,7 @@ package io.bazel.rulesscala.specs2
 import java.util.regex.Pattern
 
 import io.bazel.rulesscala.test_discovery.FilteredRunnerBuilder.FilteringRunnerBuilder
-import io.bazel.rulesscala.test_discovery._
+import io.bazel.rulesscala.test_discovery.{FilteredRunnerBuilder, JUnitFilteringRunnerBuilder, PrefixSuffixTestDiscoveringSuite}
 import org.junit.runner.notification.RunNotifier
 import org.junit.runner.{Description, RunWith}
 import org.junit.runners.Suite
@@ -34,23 +34,45 @@ object Specs2FilteringRunnerBuilder {
 class Specs2ClassRunner(testClass: Class[_], testFilter: Pattern)
   extends org.specs2.runner.JUnitRunner(testClass) {
 
-  private def specs2ExamplesMatching(pattern: Pattern, children: List[Description]): List[Description] = //still pattern
-    children.flatMap { c => c :: specs2ExamplesMatching(pattern, c.getChildren.asScala.toList) } //why are you flattening? why is this c and the other one is d, they're the same type
-      .filter(d => {
-      val testCase = d.getClassName + "#" + d.getMethodName
-      pattern.matcher(testCase).matches
-    })
-
   private def toDisplayName(d: Description) = d.getMethodName.split("::").reverse.headOption.getOrElse(d.getMethodName)
 
-  def translate(junitDescription: Description, using: Pattern): Option[String] = //using unused
-    specs2ExamplesMatching(testFilter, this.getDescription.getChildren.asScala.toList)
+  /**
+    * Turns a JUnit description structure into a flat list:
+    *
+    *  given:
+    *  class MyTest extends ...{
+    *    "This example" should {
+    *      "Do stuff" in {
+    *        ...
+    *      }
+  *      }
+    *  }
+    *
+    *  Is represented as the following description:
+    *
+    *  MyTest
+    *  |> MyTest#This example should
+    *     |> MyTest#This example should::Do stuff
+    *
+    *  This function returns a flat list of the descriptions and their children, starting with the root.
+    */
+  private def flattenDescription(description: Description): List[Description] =
+    description.getChildren.asScala.toList.flatMap(d => d :: flattenDescription(d))
+
+  private def matching(testFilter: Pattern): Description => Boolean = { d =>
+    val testCase = d.getClassName + "#" + d.getMethodName
+    testFilter.matcher(testCase).matches
+  }
+
+  private def specs2ExamplesMatching(testFilter: Pattern, junitDescription: Description): Option[String] =
+    flattenDescription(junitDescription)
+      .filter(matching(testFilter))
       .map(toDisplayName)
       .mkString(",")
       .toOption
 
   override def runWithEnv(n: RunNotifier, env: Env): Action[Stats] = {
-    val specs2MatchedExamples = translate(junitDescription = this.getDescription, using = testFilter)
+    val specs2MatchedExamples = specs2ExamplesMatching(testFilter, getDescription)
 
     // todo escape fragment if needed (e.g. contains + or other regex-specific characters)
     val newArgs = Arguments(select = Select(_ex = specs2MatchedExamples), commandLine = CommandLine.create(testClass.getName))
@@ -62,5 +84,4 @@ class Specs2ClassRunner(testClass: Class[_], testFilter: Pattern)
   private implicit class `Empty String to Option`(s: String) {
     def toOption: Option[String] = if (s.isEmpty) None else Some(s)
   }
-
 }
