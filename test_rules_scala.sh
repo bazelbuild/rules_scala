010f57a82c25a23d908e408d9acef9dd05563d41
Andy Scott <andy.g.scott@gmail.com>
2018-04-10 16:58:03 -0700

    Spin up separate reproducibility Travis job

--- test_rules_scala.sh
+++ test_rules_scala.sh
@@ -16,28 +16,6 @@ test_disappearing_class() {
   fi
   set -e
 }
-md5_util() {
-if [[ "$OSTYPE" == "darwin"* ]]; then
-   _md5_util="md5"
-else
-   _md5_util="md5sum"
-fi
-echo "$_md5_util"
-}
-
-non_deploy_jar_md5_sum() {
-  find bazel-bin/test -name "*.jar" ! -name "*_deploy.jar" | xargs -n 1 -P 5 $(md5_util) | sort
-}
-
-test_build_is_identical() {
-  bazel build test/...
-  non_deploy_jar_md5_sum > hash1
-  bazel clean
-  sleep 2 # to make sure that if timestamps slip in we get different ones
-  bazel build test/...
-  non_deploy_jar_md5_sum > hash2
-  diff hash1 hash2
-}
 
 test_transitive_deps() {
   set +e
@@ -813,8 +791,3 @@ $runner test_scala_import_library_passes_labels_of_direct_deps
 $runner java_toolchain_javacopts_are_used
 $runner bazel run test/src/main/scala/scala/test/classpath_resources:classpath_resource
 $runner test_scala_classpath_resources_expect_warning_on_namespace_conflict
-
-# This test is last since it compares the current outputs to new ones to make sure they're identical
-# If it runs before some of the above (like jmh) the "current" output in CI might be too close in time to the "new" one
-# The test also adds sleep by itself but it's best if it's last
-$runner test_build_is_identical
