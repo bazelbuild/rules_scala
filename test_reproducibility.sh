606ccaeb3de6ac88c73dba69f87767ab43e7707b
andy g scott ☜ <andyscott@users.noreply.github.com>
2018-04-10 18:05:07 -0700

    Enable local cache by default (#478)
    * Enable local cache by default
    
    * Spin up separate reproducibility Travis job

---
+++ test_reproducibility.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+md5_util() {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        _md5_util="md5"
+    else
+        _md5_util="md5sum"
+    fi
+    echo "$_md5_util"
+}
+
+non_deploy_jar_md5_sum() {
+    find bazel-bin/test -name "*.jar" ! -name "*_deploy.jar" | xargs -n 1 -P 5 $(md5_util) | sort
+}
+
+test_build_is_identical() {
+    bazel build test/...
+    non_deploy_jar_md5_sum > hash1
+    bazel clean
+    sleep 2 # to make sure that if timestamps slip in we get different ones
+    bazel build --noexperimental_local_disk_cache test/...
+    non_deploy_jar_md5_sum > hash2
+    diff hash1 hash2
+}
+
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# shellcheck source=./test_runner.sh
+. "${dir}"/test_runner.sh
+runner=$(get_test_runner "${1:-local}")
+
+
+# This test is last/separate since it compares the current outputs to new ones to make sure they're identical
+# If it runs before some of the above (like jmh) the "current" output in CI might be too close in time to the "new" one
+# The test also adds sleep by itself but it's best if it's last
+$runner test_build_is_identical
