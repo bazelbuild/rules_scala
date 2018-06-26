d45943d7c6391e5e2fc2ab2f05f97dcbfee36cf8
Johan Jern <johanjern95@gmail.com>
2018-06-26 11:21:41 -0700

    Remove temp test directory

--- test/crossbuild/HelloWorldLib211.scala
+++
@@ -1,9 +0,0 @@
-package scala.test.crossbuild
-
-trait Hello {
-  def hello: String = "hfnos"
-}
-
-trait World {
-  def world: String = "wptoh"
-}
--- test/crossbuild/ScalaBinary.scala
+++
@@ -1,7 +0,0 @@
-package scala.test.crossbuild
-
-object ScalaBinary {
-  def main(args: Array[String]) {
-    println("Hello")
-  }
-}
