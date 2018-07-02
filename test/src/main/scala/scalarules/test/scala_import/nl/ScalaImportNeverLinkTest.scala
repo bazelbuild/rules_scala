e0bb5aef32e178a8225eec62ceae2a700548d423
Ittai Zeidman <ittaiz@wix.com>
2018-07-02 07:34:01 +0300

    replace specs2 throwA matcher with manual assertion

--- test/src/main/scala/scalarules/test/scala_import/nl/ScalaImportNeverLinkTest.scala
+++ test/src/main/scala/scalarules/test/scala_import/nl/ScalaImportNeverLinkTest.scala
@@ -8,7 +8,12 @@ class ScalaImportNeverLinkTest extends SpecificationWithJUnit {
       //ScalaImportNeverLink class is packaged in scala_import_never_link.jar. Since the scala_import target
       //is marked as "neverlink" - this test class/target will be built successfully but will fail on runtime with
       //NoClassDefFoundError (neverlink targets are not available on runtime only on build/compile)
-      createScalaImportNeverLink() must throwA[NoClassDefFoundError]("scalarules/test/scala_import/nl/ScalaImportNeverLink")
+      try {
+        createScalaImportNeverLink()
+        failure("expected NoClassDefFoundError since ScalaImportNeverLink shouldn't be on runtime classpath but call succeeded")
+      } catch {
+        case e: NoClassDefFoundError if e.getMessage.contains("scalarules/test/scala_import/nl/ScalaImportNeverLink") => success
+      }
     }
   }
 
