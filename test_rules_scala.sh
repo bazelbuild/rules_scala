10ae03b84928784aebee896b6abcb26a61af9190
Johan Jern <johanjern95@gmail.com>
2018-07-04 13:03:19 -0700

    Run skipped scrooge test scala_binaries

--- test_rules_scala.sh
+++ test_rules_scala.sh
@@ -808,6 +808,8 @@ $runner bazel build "test/... --strict_java_deps=ERROR"
 $runner bazel test "test/... --strict_java_deps=ERROR"
 $runner bazel run test/src/main/scala/scalarules/test/twitter_scrooge:justscrooges
 $runner bazel run test/src/main/scala/scalarules/test/twitter_scrooge:barethrifts
+$runner bazel run test/src/main/scala/scalarules/test/twitter_scrooge:twodeep_binary
+$runner bazel run test/src/main/scala/scalarules/test/twitter_scrooge:justscrooge2b_binary
 $runner bazel run test:JavaBinary
 $runner bazel run test:JavaBinary2
 $runner bazel run test:JavaOnlySources
