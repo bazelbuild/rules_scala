3d62c07f37ab0624815519ec595b15cf7d7a3de0
Johan Jern <johanjern95@gmail.com>
2018-07-30 13:55:16 -0700

    Reset changes made to DependencyAnalyzer.scala

--- third_party/dependency_analyzer/src/main/io/bazel/rulesscala/dependencyanalyzer/DependencyAnalyzer.scala
+++ third_party/dependency_analyzer/src/main/io/bazel/rulesscala/dependencyanalyzer/DependencyAnalyzer.scala
@@ -5,6 +5,7 @@ import scala.tools.nsc.plugins.{Plugin, PluginComponent}
 import scala.tools.nsc.{Global, Phase}
 
 class DependencyAnalyzer(val global: Global) extends Plugin {
+
   val name = "dependency-analyzer"
   val description =
     "Analyzes the used dependencies and fails the compilation " +
@@ -16,27 +17,22 @@ class DependencyAnalyzer(val global: Global) extends Plugin {
   var analyzerMode: String = "error"
   var currentTarget: String = "NA"
 
-  def decodeTarget(target: String): String = target.replace(";", ":")
-
-  override def init(options: List[String], error: (String) => Unit): Boolean = {
+  override def processOptions(options: List[String], error: (String) => Unit): Unit = {
     var indirectJars: Seq[String] = Seq.empty
     var indirectTargets: Seq[String] = Seq.empty
 
     for (option <- options) {
       option.split(":").toList match {
-        case "direct-jars" :: data => direct = data.map(decodeTarget).toSet
+        case "direct-jars" :: data => direct = data.toSet
         case "indirect-jars" :: data => indirectJars = data;
-        case "indirect-targets" :: data => indirectTargets = data.map(decodeTarget)
-        case "current-target" :: target => currentTarget = target.map(decodeTarget).head
+        case "indirect-targets" :: data => indirectTargets = data.map(_.replace(";", ":"))
+        case "current-target" :: target => currentTarget = target.map(_.replace(";", ":")).head
         case "mode" :: mode => analyzerMode = mode.head
         case unknown :: _ => error(s"unknown param $unknown")
         case Nil =>
       }
     }
-
     indirect = indirectJars.zip(indirectTargets).toMap
-
-    true
   }
 
 
@@ -48,64 +44,67 @@ class DependencyAnalyzer(val global: Global) extends Plugin {
 
     override val runsAfter = List("jvm")
 
-    val phaseName: String = DependencyAnalyzer.this.name
-
-    private def warnOrError(messages: Set[String]): Unit = analyzerMode match {
-      case "error" => messages.foreach(reporter.error(NoPosition, _))
-      case "warn" => messages.foreach(reporter.warning(NoPosition, _))
-    }
+    val phaseName = DependencyAnalyzer.this.name
 
     override def newPhase(prev: Phase): StdPhase = new StdPhase(prev) {
       override def run(): Unit = {
 
         super.run()
 
-        val usedJars: Set[AbstractFile] = findUsedJars
+        val usedJars = findUsedJars
 
-        warnOrError(indirectTargetsFound(usedJars))
+        warnOnIndirectTargetsFoundIn(usedJars)
       }
 
-      private def indirectTargetsFound(usedJars: Set[AbstractFile]): Set[String] = {
-        for {
-          usedJar <- usedJars
-          usedJarPath = usedJar.path
-          target <- indirect.get(usedJarPath) if !direct.contains(usedJarPath)
-        } yield
+      private def warnOnIndirectTargetsFoundIn(usedJars: Set[AbstractFile]) = {
+        for (usedJar <- usedJars;
+             usedJarPath = usedJar.path;
+             target <- indirect.get(usedJarPath) if !direct.contains(usedJarPath)) {
+          val errorMessage =
             s"""Target '$target' is used but isn't explicitly declared, please add it to the deps.
                |You can use the following buildozer command:
                |buildozer 'add deps $target' $currentTarget""".stripMargin
+
+          analyzerMode match {
+            case "error" => reporter.error(NoPosition, errorMessage)
+            case "warn" => reporter.warning(NoPosition, errorMessage)
+          }
+        }
       }
 
       override def apply(unit: CompilationUnit): Unit = ()
     }
 
-    private def findUsedJars: Set[AbstractFile] = {
-      val jars = collection.mutable.Set[AbstractFile]()
+  }
 
-      def walkTopLevels(root: Symbol): Unit = {
-        def safeInfo(sym: Symbol): Type =
-          if (sym.hasRawInfo && sym.rawInfo.isComplete) sym.info else NoType
+  import global._
 
-        def packageClassOrSelf(sym: Symbol): Symbol =
-          if (sym.hasPackageFlag && !sym.isModuleClass) sym.moduleClass else sym
+  private def findUsedJars: Set[AbstractFile] = {
+    val jars = collection.mutable.Set[AbstractFile]()
 
-        for (x <- safeInfo(packageClassOrSelf(root)).decls) {
-          if (x == root) ()
-          else if (x.hasPackageFlag) walkTopLevels(x)
-          else if (x.owner != root) { // exclude package class members
-            if (x.hasRawInfo && x.rawInfo.isComplete) {
-              val assocFile = x.associatedFile
-              if (assocFile.path.endsWith(".class") && assocFile.underlyingSource.isDefined)
-                assocFile.underlyingSource.foreach(jars += _)
-            }
+    def walkTopLevels(root: Symbol): Unit = {
+      def safeInfo(sym: Symbol): Type =
+        if (sym.hasRawInfo && sym.rawInfo.isComplete) sym.info else NoType
+
+      def packageClassOrSelf(sym: Symbol): Symbol =
+        if (sym.hasPackageFlag && !sym.isModuleClass) sym.moduleClass else sym
+
+      for (x <- safeInfo(packageClassOrSelf(root)).decls) {
+        if (x == root) ()
+        else if (x.hasPackageFlag) walkTopLevels(x)
+        else if (x.owner != root) { // exclude package class members
+          if (x.hasRawInfo && x.rawInfo.isComplete) {
+            val assocFile = x.associatedFile
+            if (assocFile.path.endsWith(".class") && assocFile.underlyingSource.isDefined)
+              assocFile.underlyingSource.foreach(jars += _)
           }
         }
       }
-
-      exitingTyper {
-        walkTopLevels(RootClass)
-      }
-      jars.toSet
     }
+
+    exitingTyper {
+      walkTopLevels(RootClass)
+    }
+    jars.toSet
   }
 }
