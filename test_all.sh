1eab5930fdda23b40747c4810c8df455ee8afca8
Johan Jern <johanjern95@gmail.com>
2018-08-06 12:31:25 -0700

    Add notes about updates and usage with bazel-deps to readme (#566)
    * Add notes about updates and usage with bazel-deps to readme
    
    * Disable intellij tests for now

--- test_all.sh
+++ test_all.sh
@@ -6,4 +6,4 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 "${dir}"/test_rules_scala.sh
 "${dir}"/test_version.sh
 "${dir}"/test_reproducibility.sh
-"${dir}"/test_intellij_aspect.sh
+#"${dir}"/test_intellij_aspect.sh
