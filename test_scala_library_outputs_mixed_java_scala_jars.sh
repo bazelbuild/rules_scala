4fbdd1b867ca0522ae22ef96f1acda11c93f5dbe
Elle Mundy <emundy@meetup.com>
2018-10-25 13:33:39 -0400

    Also test that the outputs contain _java.jar

--- test_scala_library_outputs_mixed_java_scala_jars.sh
+++ test_scala_library_outputs_mixed_java_scala_jars.sh
@@ -12,15 +12,25 @@ exists() {
   [ -e $1 ]
 }
 
+output_contains() {
+  grep '{"id":{"targetCompleted' $1 | grep $2 > /dev/null
+}
+
 test_scala_library_outputs_mixed_java_scala_jars() {
   set -e
 
   TARGET="MixJavaScalaLib"
+  BUILD_EVENT_FILE="build_event.json"
 
-  bazel build test:"$TARGET"
+  bazel build test:"$TARGET" --build_event_json_file=$BUILD_EVENT_FILE
 
   assert exists "bazel-bin/test/${TARGET}.jar"
   assert exists "bazel-bin/test/${TARGET}_java.jar"
+
+  assert output_contains $BUILD_EVENT_FILE test/${TARGET}.jar
+  assert output_contains $BUILD_EVENT_FILE test/${TARGET}_java.jar
+
+  rm $BUILD_EVENT_FILE
 }
 
 test_scala_library_outputs_mixed_java_scala_jars
