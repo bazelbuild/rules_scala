667acd9439bd0b572e2f66c0fd0ce7441e968703
Ittai Zeidman <ittaiz@wix.com>
2020-01-19 15:14:03 +0200

    exposing java jar to BEP from all binary rules added a passing e2e for java jar of scala library for symmetry

--- test/shell/test_build_event_protocol.sh
+++ test/shell/test_build_event_protocol.sh
@@ -5,11 +5,12 @@ runner=$(get_test_runner "${1:-local}")
 
 scala_binary_common_jar_is_exposed_in_build_event_protocol() {
   local target=$1
+  local target_suffix=${2:-""}
   set +e
   bazel build test:$target --build_event_text_file=$target_bes.txt
-  cat $target_bes.txt | grep "test/$target.jar"
+  cat $target_bes.txt | grep "test/$target$target_suffix.jar"
   if [ $? -ne 0 ]; then
-    echo "test/$target.jar was not found in build event protocol:"
+    echo "test/$target$target_suffix.jar was not found in build event protocol:"
     cat $target_bes.txt
     rm $target_bes.txt
     exit 1
@@ -31,6 +32,26 @@ scala_junit_test_jar_is_exposed_in_build_event_protocol() {
   scala_binary_common_jar_is_exposed_in_build_event_protocol JunitTestWithDeps
 }
 
+scala_binary_java_jar_is_exposed_in_build_event_protocol() {
+  scala_binary_common_jar_is_exposed_in_build_event_protocol MixJavaScalaBinary _java
+}
+
+scala_library_java_jar_is_exposed_in_build_event_protocol() {
+  scala_binary_common_jar_is_exposed_in_build_event_protocol MixJavaScalaLib _java
+}
+
+scala_test_java_jar_is_exposed_in_build_event_protocol() {
+  scala_binary_common_jar_is_exposed_in_build_event_protocol MixJavaScalaScalaTest _java
+}
+
+junit_test_java_jar_is_exposed_in_build_event_protocol() {
+  scala_binary_common_jar_is_exposed_in_build_event_protocol MixJavaScalaJunitTest _java
+}
+
 $runner scala_binary_jar_is_exposed_in_build_event_protocol
 $runner scala_test_jar_is_exposed_in_build_event_protocol
 $runner scala_junit_test_jar_is_exposed_in_build_event_protocol
+$runner scala_binary_java_jar_is_exposed_in_build_event_protocol
+$runner scala_library_java_jar_is_exposed_in_build_event_protocol
+$runner scala_test_java_jar_is_exposed_in_build_event_protocol
+$runner junit_test_java_jar_is_exposed_in_build_event_protocol
