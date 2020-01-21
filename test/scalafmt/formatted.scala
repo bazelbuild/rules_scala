93de44876b8290191b994144c347f0980c2ac63b
Bor Kae Hwang <borkaehw@lucidchart.com>
2020-01-21 16:31:01 -0700

    Change default conf

--- test/scalafmt/formatted.scala
+++ test/scalafmt/formatted.scala
@@ -1,15 +1,12 @@
 package scalarules.test.scalafmt
 object Format {
   def main(args: Array[String]) {
-    val greeting: String =
-      "Hello, world!"
-    val warning1 = "Be careful with this test"
-    val warning2 = "小心這個測試"
-    val warning3 = "このテストに注意してください"
-    val warning4 = "이 시험에 조심하십시오"
-    val warning5 = "كن حذرا مع هذا الاختبار"
-    val warning6 = "Hãy cẩn thận với bài kiểm tra này"
-    val warning7 = "Будьте осторожны с этим тестом"
-    val warning8 = "😁✊🚀🍟💯"
+    val warnings: String = List(
+      "Be careful with this test",
+      "小心這個測試",
+      "このテストに注意してください",
+      "이 시험에 조심하십시오",
+      "😁✊🚀🍟💯", //mind the trailing commas
+    )
   }
 }
