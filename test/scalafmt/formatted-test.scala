f71478fc9a9c93883bc46b07d2a314c4be615b12
Bor Kae Hwang <borkaehw@lucidchart.com>
2020-01-21 15:25:04 -0700

    Change to true

--- test/scalafmt/formatted-test.scala
+++ test/scalafmt/formatted-test.scala
@@ -1,8 +1,7 @@
 package scalarules.test.scalafmt
 import org.scalatest._
 class FormatTest extends FlatSpec {
-  val message = "We will format this test!"
   "FormatTest" should "be formatted" in {
-    assert(message.equals("We will format this test!"))
+    assert(true)
   }
 }
