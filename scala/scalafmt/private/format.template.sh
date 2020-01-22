1334d7ac629c24f48287c0e85c9a7af98419c4d9
Bor Kae Hwang <borkaehw@lucidchart.com>
2020-01-22 16:29:50 -0700

    Remove unnecessary code

--- scala/scalafmt/private/format.template.sh
+++ scala/scalafmt/private/format.template.sh
@@ -1,33 +1,14 @@
 #!/bin/bash -e
 RUNPATH="${TEST_SRCDIR-$0.runfiles}"/%workspace%
 WORKSPACE_ROOT="${1:-$BUILD_WORKSPACE_DIRECTORY}"
+NONDEFAULTPATH=(${RUNPATH//bin/ })
+NONDEFAULTPATH="${NONDEFAULTPATH[0]}"bin
 
-if [ -f "$RUNPATH"/%manifest% ]; then
-    while read original formatted; do
-        if [[ ! -z "$original" ]] && [[ ! -z "$formatted" ]]; then
-            if ! cmp -s "$RUNPATH/$original" "$RUNPATH/$formatted"; then
-                if [ -z "$WORKSPACE_ROOT" ]; then
-                    echo "$original"
-                    diff "$RUNPATH/$original" "$RUNPATH/$formatted" || true
-                    EXIT=1
-                else
-                    echo "Formatting $original"
-                    cp "$RUNPATH/$formatted" "$WORKSPACE_ROOT/$original"
-                fi
-            fi
+while read original formatted; do
+    if [[ ! -z "$original" ]] && [[ ! -z "$formatted" ]]; then
+        if ! cmp -s "$WORKSPACE_ROOT/$original" "$NONDEFAULTPATH/$formatted"; then
+            echo "Formatting $original"
+            cp "$NONDEFAULTPATH/$formatted" "$WORKSPACE_ROOT/$original"
         fi
-    done < "$RUNPATH"/%manifest%
-else
-    NONDEFAULTPATH=(${RUNPATH//bin/ })
-    NONDEFAULTPATH="${NONDEFAULTPATH[0]}"bin
-    while read original formatted; do
-        if [[ ! -z "$original" ]] && [[ ! -z "$formatted" ]]; then
-            if ! cmp -s "$WORKSPACE_ROOT/$original" "$NONDEFAULTPATH/$formatted"; then
-                echo "Formatting $original"
-                cp "$NONDEFAULTPATH/$formatted" "$WORKSPACE_ROOT/$original"
-            fi
-        fi
-    done < "$NONDEFAULTPATH"/%manifest%
-fi
-
-exit $EXIT
+    fi
+done < "$NONDEFAULTPATH"/%manifest%
