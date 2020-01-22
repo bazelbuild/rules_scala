b90c10ecb926d66dfad105d431e455a295e8d8ce
Bor Kae Hwang <borkaehw@lucidchart.com>
2020-01-22 11:14:05 -0700

    Better handle failing case

--- test/shell/test_scalafmt.sh
+++ test/shell/test_scalafmt.sh
@@ -4,17 +4,17 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 . "${dir}"/test_helper.sh
 runner=$(get_test_runner "${1:-local}")
 
-backup_unformatted_file() {
+backup_unformatted() {
   FILE_PATH=$1
   FILENAME=$2
-  cp $FILE_PATH/unformatted/un${FILENAME}.scala $FILE_PATH/unformatted/un${FILENAME}.backup.scala
+  cp $FILE_PATH/unformatted/unformatted-$FILENAME.scala $FILE_PATH/unformatted/unformatted-$FILENAME.backup.scala
 }
 
-restore_unformatted_file_before_exit() {
+restore_unformatted_before_exit() {
   FILE_PATH=$1
   FILENAME=$2
-  cp $FILE_PATH/unformatted/un${FILENAME}.backup.scala $FILE_PATH/unformatted/un${FILENAME}.scala
-  rm -f $FILE_PATH/unformatted/un${FILENAME}.backup.scala
+  cp $FILE_PATH/unformatted/unformatted-$FILENAME.backup.scala $FILE_PATH/unformatted/unformatted-$FILENAME.scala
+  rm -f $FILE_PATH/unformatted/unformatted-$FILENAME.backup.scala
 }
 
 run_formatting() {
@@ -22,13 +22,8 @@ run_formatting() {
 
   FILE_PATH="$( dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) )"/scalafmt
   RULE_TYPE=$1
-  FILENAME="formatted"
-  if [[ $RULE_TYPE = test ]]; then
-    FILENAME="formatted-test"
-  elif [[ $RULE_TYPE = custom-conf ]]; then
-    FILENAME="formatted-custom-conf"
-  fi
-    
+  FILENAME=$2
+
   bazel run //test/scalafmt:formatted-$RULE_TYPE.format-test
   if [ $? -ne 0 ]; then
     echo -e "${RED} formatted-$RULE_TYPE.format-test should be a formatted target. $NC"
@@ -41,37 +36,37 @@ run_formatting() {
     exit 1
   fi
 
-  backup_unformatted_file $FILE_PATH $FILENAME
+  backup_unformatted $FILE_PATH $FILENAME
   # format unformatted*.scala
   bazel run //test/scalafmt:unformatted-$RULE_TYPE.format
   if [ $? -ne 0 ]; then
     echo -e "${RED} unformatted-$RULE_TYPE.format should run formatting. $NC"
-    restore_unformatted_file_before_exit $FILE_PATH $FILENAME
+    restore_unformatted_before_exit $FILE_PATH $FILENAME
     exit 1
   fi
 
-  diff $FILE_PATH/unformatted/un${FILENAME}.scala $FILE_PATH/formatted/${FILENAME}.scala
+  diff $FILE_PATH/unformatted/unformatted-$FILENAME.scala $FILE_PATH/formatted/formatted-$FILENAME.scala
   if [ $? -ne 0 ]; then
-    echo -e "${RED} un${FILENAME}.scala should be the same as ${FILENAME}.scala after formatting. $NC"
-    restore_unformatted_file_before_exit $FILE_PATH $FILENAME
+    echo -e "${RED} unformatted-$FILENAME.scala should be the same as formatted-$FILENAME.scala after formatting. $NC"
+    restore_unformatted_before_exit $FILE_PATH $FILENAME
     exit 1
   fi
-  restore_unformatted_file_before_exit $FILE_PATH $FILENAME
+  restore_unformatted_before_exit $FILE_PATH $FILENAME
 }
 
 test_scalafmt_binary() {
-  run_formatting binary
+  run_formatting binary encoding
 }
 
 test_scalafmt_library() {
-  run_formatting library
+  run_formatting library encoding
 }
 
 test_scalafmt_test() {
-  run_formatting test
+  run_formatting test test
 }
 test_custom_conf() {
-  run_formatting custom-conf
+  run_formatting custom-conf custom-conf
 }
 
 $runner test_scalafmt_binary
