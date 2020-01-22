2d7d21853e8c61a4b6fefceb268d9e363cc1d693
simuons <simonasp@wix.com>
2020-01-22 16:35:28 +0200

    more test cases

--- test/proto/PackProtosTest.scala
+++ test/proto/PackProtosTest.scala
@@ -1,7 +1,13 @@
-
 class PackProtosTest extends org.scalatest.FlatSpec {
-
   "scala_proto_library" should "pack input proto next to generated code" in {
-    assert(getClass.getResource("test/proto/test2.proto") != null)
+    assert(getClass.getResource("test/proto/standalone.proto") != null)
+    assert(getClass.getResource("proto/standalone.proto") != null)
+    assert(getClass.getResource("standalone.proto") != null)
+    assert(getClass.getResource("prefix/test/proto/standalone.proto") != null)
+    assert(getClass.getResource("prefix/proto/standalone.proto") != null)
+    assert(getClass.getResource("test/proto/some/path/nested.proto") != null)
+    assert(getClass.getResource("path/nested.proto") != null)
+    assert(getClass.getResource("prefix/test/proto/some/path/nested.proto") != null)
+    assert(getClass.getResource("prefix/path/nested.proto") != null)
   }
 }
