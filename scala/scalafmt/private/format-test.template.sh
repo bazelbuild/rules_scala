7585fbf57395e4c2d846ad9261166f2e55d6be1f
Bor Kae Hwang <borkaehw@lucidchart.com>
2020-01-23 12:02:56 -0700

    Change to RUNPATH

--- scala/scalafmt/private/format-test.template.sh
+++ scala/scalafmt/private/format-test.template.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -e
-RUNPATH="${TEST_SRCDIR-$0.runfiles}"/%workspace%
 WORKSPACE_ROOT="${1:-$BUILD_WORKSPACE_DIRECTORY}"
-NONDEFAULTPATH=(${RUNPATH//bin/ })
-NONDEFAULTPATH="${NONDEFAULTPATH[0]}"bin
+RUNPATH="${TEST_SRCDIR-$0.runfiles}"/%workspace%
+RUNPATH=(${RUNPATH//bin/ })
+RUNPATH="${RUNPATH[0]}"bin
 
 EXIT=0
 while read original formatted; do
     if [[ ! -z "$original" ]] && [[ ! -z "$formatted" ]]; then
-        if ! cmp -s "$WORKSPACE_ROOT/$original" "$NONDEFAULTPATH/$formatted"; then
+        if ! cmp -s "$WORKSPACE_ROOT/$original" "$RUNPATH/$formatted"; then
             echo $original
-            diff "$WORKSPACE_ROOT/$original" "$NONDEFAULTPATH/$formatted" || true
+            diff "$WORKSPACE_ROOT/$original" "$RUNPATH/$formatted" || true
             EXIT=1
         fi
     fi
-done < "$NONDEFAULTPATH"/%manifest%
+done < "$RUNPATH"/%manifest%
 
 exit $EXIT
--- scala/scalafmt/private/format.template.sh
+++ scala/scalafmt/private/format.template.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
-RUNPATH="${TEST_SRCDIR-$0.runfiles}"/%workspace%
 WORKSPACE_ROOT="${1:-$BUILD_WORKSPACE_DIRECTORY}"
-NONDEFAULTPATH=(${RUNPATH//bin/ })
-NONDEFAULTPATH="${NONDEFAULTPATH[0]}"bin
+RUNPATH="${TEST_SRCDIR-$0.runfiles}"/%workspace%
+RUNPATH=(${RUNPATH//bin/ })
+RUNPATH="${RUNPATH[0]}"bin
 
 while read original formatted; do
     if [[ ! -z "$original" ]] && [[ ! -z "$formatted" ]]; then
-        if ! cmp -s "$WORKSPACE_ROOT/$original" "$NONDEFAULTPATH/$formatted"; then
+        if ! cmp -s "$WORKSPACE_ROOT/$original" "$RUNPATH/$formatted"; then
             echo "Formatting $original"
-            cp "$NONDEFAULTPATH/$formatted" "$WORKSPACE_ROOT/$original"
+            cp "$RUNPATH/$formatted" "$WORKSPACE_ROOT/$original"
         fi
     fi
-done < "$NONDEFAULTPATH"/%manifest%
+done < "$RUNPATH"/%manifest%
