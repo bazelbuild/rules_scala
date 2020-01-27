542d89aa95b08e1a83b2b34feb9ff7cad79b6552
Andy Scott <andy.g.scott@gmail.com>
2020-01-26 17:29:59 -0800

    fix

--- src/scala/io/bazel/rules_scala/discover_tests_worker/DiscoverTestsWorker.scala
+++ src/scala/io/bazel/rules_scala/discover_tests_worker/DiscoverTestsWorker.scala
@@ -1,12 +1,12 @@
-package io.bazel.rules_scala.test_discovery
+package io.bazel.rules_scala.discover_tests_worker
 
 import io.bazel.rules_scala.worker.Worker
 
 import io.github.classgraph.ClassGraph
 
-object TestDiscoveryWorker extends Worker.Interface {
+object DiscoverTestsWorker extends Worker.Interface {
 
-  def main(args: Array[String]): Unit = Worker.workerMain(args, TestDiscoveryWorker)
+  def main(args: Array[String]): Unit = Worker.workerMain(args, DiscoverTestsWorker)
 
   def work(args: Array[String]): Unit = {
     println("WORK IT")
