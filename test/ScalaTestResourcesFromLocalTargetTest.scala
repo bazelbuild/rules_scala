d260d0179f6fb1bea827af800baa2d3e8b342088
David Haxton <davidhaxton@asana.com>
2020-02-28 11:37:57 -0800

    nits

--- test/ScalaTestResourcesFromLocalTargetTest.scala
+++ test/ScalaTestResourcesFromLocalTargetTest.scala
@@ -1,8 +1,9 @@
 package scalarules.test
 
 class ScalaTestResourcesFromLocalTargetTest extends org.scalatest.FlatSpec {
-  "scala_test's resources when referencing local target" should "assert that local target is not placed in bazel-out, but rather next to the generated code" in {
-    assert(getClass.getResource("/bazel-out/darwin-fastbuild/bin/test/py_resource_binary") == null)
-    // assert(getClass.getResource("/test/py_resource_binary") != null)
-  }
+  "scala_test's resources when referencing local target" should 
+    "assert that local target is not placed in bazel-out, but rather next to the packaged code" in {
+      assert(getClass.getResource("/bazel-out/darwin-fastbuild/bin/test/py_resource_binary") == null)
+      // assert(getClass.getResource("/test/py_resource_binary") != null)
+    }
 }
