874c74673973ac2005acb20a978d03fdd5dac297
Gergely Fábián <gergo.fb@gmail.com>
2020-04-30 15:28:48 +0200

    Reproduce "File name too long" exception with new JacocoInstrumenter implementation

---
+++ test/coverage/D1.scala
@@ -0,0 +1,46 @@
+package coverage
+
+object D1 {
+  def veryLongFunctionNameIsHereAaaaaaaaa(): Unit = {
+    val l = List("a")
+    // This is bad style, but sometimes still happens to Scala developers...
+    // For comprehension that will generate classes with very long names (when compiled).
+    for {
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+      _ <- l
+    } yield {
+      ()
+    }
+  }
+}
