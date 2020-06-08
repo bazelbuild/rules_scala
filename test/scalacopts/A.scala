48da7a025cffd7b3e7000b9d20e77713f7f9c11d
wisechengyi <wisechengyi@gmail.com>
2020-06-07 21:19:08 -0700

    comment

--- test/scalacopts/A.scala
+++ test/scalacopts/A.scala
@@ -8,6 +8,7 @@ object Macros {
 
   def macroSettings(c: blackbox.Context): c.Expr[String] = {
     import c.universe._
+    // c.settings are the values from scalac's -Xmacro-settings
     val s = c.settings.mkString(",")
     c.Expr(q"""${s}""")
   }
