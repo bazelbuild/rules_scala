bazelbuild/rules_scala

commit 96f9d94cdbdcc09422a0aa6d9a7aeb256c181b26

unify all of the default info phases implementations (#958)
* unify all of the default info phases implementations

* collect_data = True

scala/private/phases/phase_default_info.bzl
@@ -3,34 +3,39 @@
 #
 # DOCUMENT THIS
 #
-def phase_default_info_binary(ctx, p):
-    return struct(
-        external_providers = {
-            "DefaultInfo": DefaultInfo(
-                executable = p.declare_executable,
-                files = depset([p.declare_executable] + p.compile.full_jars),
-                runfiles = p.runfiles.runfiles,
-            ),
-        },
-    )
 
-def phase_default_info_library(ctx, p):
-    return struct(
-        external_providers = {
-            "DefaultInfo": DefaultInfo(
-                files = depset(p.compile.full_jars),
-                runfiles = p.runfiles.runfiles,
-            ),
-        },
-    )
+def phase_default_info(ctx, p):
+    executable = None
+    files = []
+    runfiles = []
 
-def phase_default_info_scalatest(ctx, p):
+    phase_names = dir(p)
+    phase_names.remove("to_json")
+    phase_names.remove("to_proto")
+    for phase_name in phase_names:
+        phase = getattr(p, phase_name)
+
+        if hasattr(phase, "executable"):
+            if executable == None:
+                executable = phase.executable
+            else:
+                fail("only one executable may be provided")
+
+        if hasattr(phase, "files"):
+            files.append(phase.files)
+
+        if hasattr(phase, "runfiles"):
+            runfiles.append(phase.runfiles)
+
     return struct(
         external_providers = {
             "DefaultInfo": DefaultInfo(
-                executable = p.declare_executable,
-                files = depset([p.declare_executable] + p.compile.full_jars),
-                runfiles = ctx.runfiles(p.coverage_runfiles.coverage_runfiles, transitive_files = p.runfiles.runfiles.files),
+                executable = executable,
+                files = depset(transitive = files),
+                # TODO:
+                # Per Bazel documentation, we should avoid using collect_data. The core phases need to be updated
+                # before we can make the adjustment.
+                runfiles = ctx.runfiles(transitive_files = depset(transitive = runfiles), collect_data = True),
             ),
         },
     )

scala/private/phases/phase_compile.bzl
@@ -165,6 +165,7 @@ def _phase_compile(
         class_jar = out.class_jar,
         coverage = out.coverage.external,
         full_jars = out.full_jars,
+        files = depset(out.full_jars),
         ijar = out.ijar,
         ijars = out.ijars,
         rjars = depset(out.full_jars, transitive = [rjars]),

scala/private/phases/phase_coverage_runfiles.bzl
@@ -24,5 +24,6 @@ def phase_coverage_runfiles(ctx, p):
         coverage_runfiles = ctx.files._jacocorunner + ctx.files._lcov_merger + coverage_replacements.values()
     return struct(
         coverage_runfiles = coverage_runfiles,
+        runfiles = depset(coverage_runfiles),
         rjars = rjars,
     )

scala/private/phases/phase_declare_executable.bzl
@@ -10,6 +10,10 @@ load(
 
 def phase_declare_executable(ctx, p):
     if (is_windows(ctx)):
-        return ctx.actions.declare_file("%s.exe" % ctx.label.name)
+        return struct(
+            executable = ctx.actions.declare_file("%s.exe" % ctx.label.name),
+        )
     else:
-        return ctx.actions.declare_file(ctx.label.name)
+        return struct(
+            executable = ctx.actions.declare_file(ctx.label.name),
+        )

scala/private/phases/phase_runfiles.bzl
@@ -24,7 +24,7 @@ def phase_runfiles_scalatest(ctx, p):
 
     args = struct(
         transitive_files = depset(
-            [p.declare_executable, p.java_wrapper] + ctx.files._java_runtime + runfiles_ext,
+            [p.java_wrapper] + ctx.files._java_runtime + runfiles_ext,
             transitive = [p.compile.rjars],
         ),
         args_file = args_file,
@@ -38,7 +38,7 @@ def _phase_runfiles_default(ctx, p, _args = struct()):
     return _phase_runfiles(
         ctx,
         _args.transitive_files if hasattr(_args, "transitive_files") else depset(
-            [p.declare_executable, p.java_wrapper] + ctx.files._java_runtime,
+            [p.java_wrapper] + ctx.files._java_runtime,
             transitive = [p.compile.rjars],
         ),
         _args.args_file if hasattr(_args, "args_file") else None,
@@ -49,10 +49,7 @@ def _phase_runfiles(
         transitive_files,
         args_file):
     return struct(
-        runfiles = ctx.runfiles(
-            transitive_files = transitive_files,
-            collect_data = True,
-        ),
+        runfiles = transitive_files,
         args_file = args_file,
     )
 

scala/private/phases/phase_write_executable.bzl
@@ -67,7 +67,7 @@ def _phase_write_executable(
         jvm_flags,
         use_jacoco,
         main_class):
-    executable = p.declare_executable
+    executable = p.declare_executable.executable
     wrapper = p.java_wrapper
 
     if (is_windows(ctx)):

scala/private/phases/phases.bzl
@@ -45,12 +45,7 @@ load(
     _phase_runfiles_library = "phase_runfiles_library",
     _phase_runfiles_scalatest = "phase_runfiles_scalatest",
 )
-load(
-    "@io_bazel_rules_scala//scala/private:phases/phase_default_info.bzl",
-    _phase_default_info_binary = "phase_default_info_binary",
-    _phase_default_info_library = "phase_default_info_library",
-    _phase_default_info_scalatest = "phase_default_info_scalatest",
-)
+load("@io_bazel_rules_scala//scala/private:phases/phase_default_info.bzl", _phase_default_info = "phase_default_info")
 load("@io_bazel_rules_scala//scala/private:phases/phase_scalac_provider.bzl", _phase_scalac_provider = "phase_scalac_provider")
 load("@io_bazel_rules_scala//scala/private:phases/phase_write_manifest.bzl", _phase_write_manifest = "phase_write_manifest")
 load("@io_bazel_rules_scala//scala/private:phases/phase_collect_srcjars.bzl", _phase_collect_srcjars = "phase_collect_srcjars")
@@ -127,9 +122,7 @@ phase_runfiles_scalatest = _phase_runfiles_scalatest
 phase_runfiles_common = _phase_runfiles_common
 
 # default_info
-phase_default_info_binary = _phase_default_info_binary
-phase_default_info_library = _phase_default_info_library
-phase_default_info_scalatest = _phase_default_info_scalatest
+phase_default_info = _phase_default_info
 
 # scalafmt
 phase_scalafmt = _phase_scalafmt

scala/private/rules/scala_binary.bzl
@@ -15,7 +15,7 @@ load(
     "phase_collect_jars_common",
     "phase_compile_binary",
     "phase_declare_executable",
-    "phase_default_info_binary",
+    "phase_default_info",
     "phase_java_wrapper_common",
     "phase_merge_jars",
     "phase_runfiles_common",
@@ -42,7 +42,7 @@ def _scala_binary_impl(ctx):
             ("merge_jars", phase_merge_jars),
             ("runfiles", phase_runfiles_common),
             ("write_executable", phase_write_executable_common),
-            ("default_info", phase_default_info_binary),
+            ("default_info", phase_default_info),
         ],
     )
 

commit d9062f929ab776a4c691fce6389529a02076038f

Add executable as a direct dependency
This fixes a bug introduced by
https://github.com/bazelbuild/rules_scala/pull/958. Before #958,
`rootpath` on a Scala binary resolved to the wrapper binary so other
rules could treat it as an opaque executable. After that PR `rootpath`
now resolves to the JAR instead of the wrapper binary.

Adding the executable back as a direct dependency fixes that.

scala/private/phases/phase_default_info.bzl
@@ -7,6 +7,7 @@
 def phase_default_info(ctx, p):
     executable = None
     files = []
+    direct = None
     runfiles = []
 
     phase_names = dir(p)
@@ -18,6 +19,7 @@ def phase_default_info(ctx, p):
         if hasattr(phase, "executable"):
             if executable == None:
                 executable = phase.executable
+                direct = [executable]
             else:
                 fail("only one executable may be provided")
 
@@ -31,11 +33,11 @@ def phase_default_info(ctx, p):
         external_providers = {
             "DefaultInfo": DefaultInfo(
                 executable = executable,
-                files = depset(transitive = files),
+                files = depset(direct = direct, transitive = files),
                 # TODO:
                 # Per Bazel documentation, we should avoid using collect_data. The core phases need to be updated
                 # before we can make the adjustment.
-                runfiles = ctx.runfiles(transitive_files = depset(transitive = runfiles), collect_data = True),
+                runfiles = ctx.runfiles(transitive_files = depset(direct = direct, transitive = runfiles), collect_data = True),
             ),
         },
     )