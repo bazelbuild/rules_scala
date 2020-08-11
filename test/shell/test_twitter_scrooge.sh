8382479325e4eb691525e21dc21004b6edd65595
Alex Beal <beala@stripe.com>
2020-08-11 13:51:18 -0600

    Set javabase and toolchain

--- test/shell/test_twitter_scrooge.sh
+++ test/shell/test_twitter_scrooge.sh
@@ -5,7 +5,12 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 runner=$(get_test_runner "${1:-local}")
 
 scrooge_compile_with_jdk_11() {
-    bazel build --javacopt='--release 11' test/src/main/scala/scalarules/test/twitter_scrooge/...
+    bazel build --javabase=@bazel_tools//tools/jdk:remote_jdk11 \
+        --host_javabase=@bazel_tools//tools/jdk:remote_jdk11 \
+        --host_java_toolchain=@bazel_tools//tools/jdk:toolchain_java11 \
+        --java_toolchain=@bazel_tools//tools/jdk:toolchain_java11 \
+        --javacopt='--release 11' \
+        test/src/main/scala/scalarules/test/twitter_scrooge/...
 }
 
 $runner scrooge_compile_with_jdk_11
