071050b64b33fde1b776eaf80d29cc75d37bf389
Vaidas Pilkauskas <vaidasp@wix.com>
2020-10-20 09:16:02 +0300

    Fix tests meaning

--- test/src/main/scala/scalarules/test/neverlink/NeverlinkTest.scala
+++ test/src/main/scala/scalarules/test/neverlink/NeverlinkTest.scala
@@ -3,11 +3,11 @@ package neverlink
 import org.scalatest._
 
 class NeverlinkTest extends FlatSpec {
-  "neverlink=True" should "exclude jar from classpath" in {
+  "neverlink=False" should "include jar into classpath" in {
     getClass.getClassLoader.loadClass("neverlink.A")
   }
 
-  "neverlink=False" should "not include jar in classpath" in {
+  "neverlink=True" should "exclude jar from classpath" in {
     assertThrows[ClassNotFoundException]{
       getClass.getClassLoader.loadClass("neverlink.B")
     }
