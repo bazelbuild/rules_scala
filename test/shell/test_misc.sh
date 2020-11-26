93e3586f6938b0d593547fee7141c69465ff3be6
Vaidas Pilkauskas <vaidasp@wix.com>
2020-11-26 09:42:03 +0200

    Move coverage tests to a separate script for convenience

--- test/shell/test_misc.sh
+++ test/shell/test_misc.sh
@@ -122,21 +122,6 @@ test_override_javabin() {
   JAVABIN=/etc/basdf action_should_fail run test:ScalaBinary
 }
 
-test_coverage_on() {
-    bazel coverage \
-          --extra_toolchains="//scala:code_coverage_toolchain" \
-          //test/coverage/...
-    diff test/coverage/expected-coverage.dat $(bazel info bazel-testlogs)/test/coverage/test-all/coverage.dat
-}
-
-test_coverage_includes_test_targets() {
-    bazel coverage \
-          --extra_toolchains="//scala:code_coverage_toolchain" \
-          --instrument_test_targets=True \
-          //test/coverage/...
-    grep -q "SF:test/coverage/TestAll.scala" $(bazel info bazel-testlogs)/test/coverage/test-all/coverage.dat
-}
-
 xmllint_test() {
   find -L ./bazel-testlogs -iname "*.xml" | xargs -n1 xmllint > /dev/null
 }
@@ -149,6 +134,4 @@ $runner test_benchmark_jmh_failure
 $runner scala_test_test_filters
 $runner test_multi_service_manifest
 $runner test_override_javabin
-$runner test_coverage_on
-$runner test_coverage_includes_test_targets
 $runner xmllint_test
