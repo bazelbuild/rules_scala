205fc54aabb9b22e64f82f5df9191f73040a8ad9
simuons <simonasp@wix.com>
2020-12-21 09:47:36 +0200

    Cleanup

--- scala/private/experiments/pipeline/Compiler.java
+++ scala/private/experiments/pipeline/Compiler.java
@@ -8,26 +8,18 @@ import scala.reflect.io.PlainFile;
 import scala.reflect.io.ZipArchive;
 import scala.tools.nsc.CompilerCommand;
 import scala.tools.nsc.Global;
-import scala.tools.nsc.Settings;
 
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static scala.collection.JavaConverters.asJavaCollection;
 import static scala.collection.JavaConverters.asScalaBuffer;
+import static scala.runtime.BoxedUnit.UNIT;
 
-/*
-    Currently this simple wraps nsc into worker.
-    Maybe there is no need for it to be so generic
-    Maybe it should be tailored for pickler.
-    TODO: Fix reporting.
-    TODO: Encapsulate pickler flags?
-    TODO: Rename to Compiler or Scalac?
-*/
-public class Compiler implements Worker.Interface {
+public final class Compiler implements Worker.Interface {
 
     public static void main(String... args) throws Exception {
         Worker.workerMain(args, new pipeline.Compiler());
@@ -35,37 +27,53 @@ public class Compiler implements Worker.Interface {
 
     @Override
     public void work(String... args) {
-        CompilerCommand command = new CompilerCommand(
-                asScalaBuffer(Arrays.asList(args)).toList(),
-                new Settings()
-        );
+        CompilerCommand command = parse(args);
+
         Global compiler = new Global(command.settings());
         Global.Run run = compiler.new Run();
 
-        List<AbstractFile> files = new ArrayList<>(command.files().length());
-
-        for (String filename : asJavaCollection(command.files())) {
-            if (filename.endsWith(".srcjar")) {
-                // TODO: Clean this up
-                // TODO: This leaks resources as nothing closes zip files
-                File file = Paths.get(filename).toFile();
-                FileZipArchive zip = ZipArchive.fromFile(file);
-
-                zip.allDirs().forEach((k, v) -> {
-                    Traversable<ZipArchive.Entry> entryTraversable = v.entries().values().filter(f -> f.hasExtension("scala") || f.hasExtension("java"));
-                    files.addAll(asJavaCollection(entryTraversable.toList()));
-                });
-
-            } else {
-                files.add(PlainFile.getFile(filename));
-            }
-        }
-
-        run.compileFiles(asScalaBuffer(files).toList());
+        run.compileFiles(files(command));
 
         if (compiler.reporter().hasErrors()) {
             compiler.reporter().flush();
         }
-
     }
+
+    private static CompilerCommand parse(String... args) {
+        return new CompilerCommand(
+                asScalaBuffer(asList(args)).toList(),
+                error -> {
+                    System.err.println(error);
+                    return UNIT;
+                }
+        );
+    }
+
+    private static scala.collection.immutable.List<AbstractFile> files(CompilerCommand command) {
+        List<AbstractFile> files = new ArrayList<>(command.files().length());
+        for (String filename : asJavaCollection(command.files())) {
+            if (filename.endsWith(".srcjar")) {
+                files.addAll(extract(filename));
+            } else {
+                files.add(PlainFile.getFile(filename));
+            }
+        }
+        return asScalaBuffer(files).toList();
+    }
+
+    private static List<AbstractFile> extract(String srcjar) {
+        List<AbstractFile> files = new ArrayList<>();
+        // TODO: Clean this up
+        // TODO: This leaks resources as nothing closes zip files
+        File file = Paths.get(srcjar).toFile();
+        FileZipArchive zip = ZipArchive.fromFile(file);
+
+        zip.allDirs().forEach((k, v) -> {
+            Traversable<ZipArchive.Entry> entryTraversable = v.entries().values().filter(f -> f.hasExtension("scala") || f.hasExtension("java"));
+            files.addAll(asJavaCollection(entryTraversable.toList()));
+        });
+
+        return files;
+    }
+
 }
