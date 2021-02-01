daacd872a203b94e6b7f25581810e0f9af4017b0
Vaidas Pilkauskas <vaidasp@wix.com>
2021-02-01 19:04:29 +0200

    Revert scala_import stamping tests

--- test/src/main/scala/scalarules/test/scala_import/ScalaImportExposesJarsTest.scala
+++ test/src/main/scala/scalarules/test/scala_import/ScalaImportExposesJarsTest.scala
@@ -20,11 +20,6 @@ class ScalaImportExposesJarsTest extends SpecificationWithJUnit {
       success
     }
 
-    "stamp jars with a target label" in {
-      findManifest[Cache[String, String]] must haveTargetLabel
-      findManifest[ArrayUtils] must haveTargetLabel
-    }
-
     "preserve existing Manifest attributes" in {
       findManifest[ArrayUtils] must haveMainAttribute("Bundle-Name")
     }
