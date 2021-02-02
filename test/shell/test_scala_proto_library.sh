c0d79f9a125686175787c6a4b3ebe71a3bdffe38
simuons <simonasp@wix.com>
2021-02-02 09:39:34 +0200

    Report exceptions from protoc generator

--- test/shell/test_scala_proto_library.sh
+++ test/shell/test_scala_proto_library.sh
@@ -20,6 +20,15 @@ test_scala_proto_custom_generator() {
   --extra_toolchains=//test/proto/custom_generator:scala_proto_toolchain
 }
 
+test_scala_proto_show_generator_exception() {
+  action_should_fail_with_message \
+    "java.lang.RuntimeException: expected generator failure" \
+    build //test/proto/custom_generator:any_proto_scala \
+    --extra_toolchains=//test/proto/custom_generator:failing_scala_proto_deps_toolchain \
+    --extra_toolchains=//test/proto/custom_generator:failing_scala_proto_toolchain
+}
+
 export USE_BAZEL_VERSION=${USE_BAZEL_VERSION:-$(cat ../../../.bazelversion)}
 $runner test_scala_proto_library_action_label
 $runner test_scala_proto_custom_generator
+$runner test_scala_proto_show_generator_exception
