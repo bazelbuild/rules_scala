1f3d243b2e41039c5b382e557006710a40e4da0f
Vaidas Pilkauskas <vaidasp@wix.com>
2021-02-12 12:44:22 +0200

    Remove unused val

--- test/src/main/scala/scalarules/test/scala_import/ScalaImportExposesJarsTest.scala
+++ test/src/main/scala/scalarules/test/scala_import/ScalaImportExposesJarsTest.scala
@@ -5,8 +5,6 @@ import org.apache.commons.lang3.ArrayUtils
 import org.specs2.mutable.SpecWithJUnit
 
 class ScalaImportExposesJarsTest extends SpecWithJUnit {
-  val targetLabel = "//test/src/main/scala/scalarules/test/scala_import:guava_and_commons_lang"
-
   "scala_import" should {
     "enable using the jars it exposes" in {
       println(classOf[Cache[String, String]])
@@ -14,5 +12,4 @@ class ScalaImportExposesJarsTest extends SpecWithJUnit {
       success
     }
   }
-
 }
