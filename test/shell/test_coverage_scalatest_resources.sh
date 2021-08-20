71e5c4d62ae057f67b0415ee8748afb43e33c672
Billy Autrey <billy.autrey@gemini.com>
2021-08-20 13:18:46 -0500

    Added resource tests to test_coverage.sh

--- test/shell/test_coverage_scalatest_resources.sh
+++ test/shell/test_coverage_scalatest_resources.sh
@@ -4,17 +4,17 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 . "${dir}"/test_helper.sh
 runner=$(get_test_runner "${1:-local}")
 
-test_coverage_on() {
+test_coverage_succeeds_resource_call() {
     bazel coverage //test/coverage_scalatest_resources/consumer:tests
     diff test/coverage_scalatest_resources/expected-coverage.dat $(bazel info bazel-testlogs)/test/coverage_scalatest_resources/consumer/tests/coverage.dat
 }
 
-test_coverage_includes_test_targets() {
+test_coverage_includes_resource_test_targets() {
     bazel coverage \
           --instrument_test_targets=True \
           //test/coverage_scalatest_resources/consumer:tests
     grep -q "SF:test/coverage_scalatest_resources/consumer/src/test/scala/com/example/consumer/ConsumerSpec.scala" $(bazel info bazel-testlogs)/test/coverage_scalatest_resources/consumer/tests/coverage.dat
 }
 
-$runner test_coverage_on
-$runner test_coverage_includes_test_targets
+$runner test_coverage_succeeds_resource_call
+$runner test_coverage_includes_resource_test_targets
