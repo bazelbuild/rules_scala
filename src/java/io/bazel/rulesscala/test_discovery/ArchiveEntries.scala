75263709ac63c03b6d004af5c615a67d9758eb2e
Vaidas Pilkauskas <vaidasp@wix.com>
2021-09-17 20:26:41 +0300

    Add directory scanning for test classes to JUnit runner
    Directory scanning is needed for such cases as Fast Build support in Intellij

---
+++ src/java/io/bazel/rulesscala/test_discovery/ArchiveEntries.scala
@@ -0,0 +1,36 @@
+package io.bazel.rulesscala.test_discovery
+
+import java.io.{File, FileInputStream}
+import java.util.jar.{JarEntry, JarInputStream}
+
+object ArchiveEntries {
+  def listClassFiles(file: File): Stream[String] = {
+    val allEntries = if (file.isDirectory)
+      directoryEntries(file).map(_.stripPrefix(file.toString).stripPrefix("/"))
+    else
+      jarEntries(new JarInputStream(new FileInputStream(file)))
+
+    allEntries.filter(_.endsWith(".class"))
+  }
+
+  private def getJarEntryOrCloseStream(jarInputStream: JarInputStream): Option[JarEntry] = {
+    val entry = Option(jarInputStream.getNextJarEntry)
+
+    if (entry.isEmpty)
+      jarInputStream.close()
+
+    entry
+  }
+
+  private def jarEntries(jarInputStream: JarInputStream): Stream[String] =
+    Stream.continually(getJarEntryOrCloseStream(jarInputStream))
+      .takeWhile(_.isDefined)
+      .flatten
+      .map(_.getName)
+
+  private def directoryEntries(file: File): Stream[String] =
+    file.toString #:: (file.listFiles match {
+      case null => Stream.empty
+      case files => files.toStream.flatMap(directoryEntries)
+    })
+}
