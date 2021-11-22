fb33093dff54db3a03993c272acd70216abb92fc
wisechengyi <wisechengyi@gmail.com>
2021-11-22 11:34:26 -0800

    add some tests

---
+++ test/resource_packaging/Test.scala
@@ -0,0 +1,11 @@
+import org.scalatest.flatspec.AnyFlatSpec
+
+class FirstSpec extends AnyFlatSpec {
+  it should "have file available to grab" in {
+    assert(getClass.getResource("/a/b/xxx.txt") != null)
+  }
+
+  it should "have directory available to grab" in {
+    assert(getClass.getResource("/a/b") != null)
+  }
+}
