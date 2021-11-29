55d91dff20564392aee1020cd207a65dac787423
Andrius Bentkus <andriusb@wix.com>
2021-11-29 13:23:23 +0200

    Revert "shorter toArray implementation"
    This reverts commit efb156051bbcbf72efddd7f8830ea31ffeae1bab.

--- src/java/io/bazel/rulesscala/worker/MultiplexWorker.java
+++ src/java/io/bazel/rulesscala/worker/MultiplexWorker.java
@@ -130,6 +130,11 @@ public final class MultiplexWorker {
   }
 
   private static String[] stringListToArray(List<String> argList) {
-    return argList.toArray(String[]::new);
+    int numArgs = argList.size();
+    String[] args = new String[numArgs];
+    for (int i = 0; i < numArgs; i++) {
+      args[i] = argList.get(i);
+    }
+    return args;
   }
 }
