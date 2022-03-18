848cbee4efea1e96494a16d1c42759cabf94070d
Justinas Simanavicius <justinass@wix.com>
2022-03-18 18:03:03 +0200

    dep analyzer 3

--- third_party/dependency_analyzer/src/main/io/bazel/rulesscala/dependencyanalyzer3/DependencyAnalyzer.scala
+++ third_party/dependency_analyzer/src/main/io/bazel/rulesscala/dependencyanalyzer3/DependencyAnalyzer.scala
@@ -1,30 +1,28 @@
 package io.bazel.rulesscala.dependencyanalyzer
 
-import dotty.tools.dotc.ast.Trees._
+import dotty.tools.dotc.ast.Trees.*
 import dotty.tools.dotc.ast.tpd
 import dotty.tools.dotc.core.Constants.Constant
 import dotty.tools.dotc.core.Contexts.Context
-import dotty.tools.dotc.core.Decorators._
-import dotty.tools.dotc.core.StdNames._
-import dotty.tools.dotc.core.Symbols._
+import dotty.tools.dotc.core.Decorators.*
+import dotty.tools.dotc.core.StdNames.*
+import dotty.tools.dotc.core.Symbols.*
 import dotty.tools.dotc.plugins.{PluginPhase, StandardPlugin}
 import dotty.tools.dotc.transform.{Pickler, Staging}
 
-class DependencyAnalyzer extends StandardPlugin {
+class DependencyAnalyzer extends StandardPlugin:
   override val name: String = "dependency-analyzer"
   override val description: String =
     "Analyzes the used dependencies. Can check and warn or fail the " +
-      "compilation for issues including not directly including " +
-      "dependencies which are directly included in the code, or " +
-      "including unused dependencies."
+    "compilation for issues including not directly including " +
+    "dependencies which are directly included in the code, or " +
+    "including unused dependencies."
 
   def init(options: List[String]): List[PluginPhase] =
     (new DependencyAnalyzerPhase) :: Nil
-}
 
-class DependencyAnalyzerPhase extends PluginPhase {
-
-  import tpd._
+class DependencyAnalyzerPhase extends PluginPhase:
+  import tpd.*
 
   val phaseName = "dependecy-analyzer-phase"
 
@@ -33,4 +31,3 @@ class DependencyAnalyzerPhase extends PluginPhase {
 
   override def transformApply(tree: Apply)(implicit ctx: Context): Tree =
     tree
-}
