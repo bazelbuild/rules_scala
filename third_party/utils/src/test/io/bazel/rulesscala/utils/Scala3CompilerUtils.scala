600147016413f19b5a6923f59e18bab6bd2caa56
Justinas Simanavicius <justinass@wix.com>
2022-03-21 09:55:37 +0200

    Cleanup

--- third_party/utils/src/test/io/bazel/rulesscala/utils/Scala3CompilerUtils.scala
+++ third_party/utils/src/test/io/bazel/rulesscala/utils/Scala3CompilerUtils.scala
@@ -2,7 +2,7 @@ package io.bazel.rulesscala.utils
 
 import dotty.tools.dotc.Compiler
 import dotty.tools.dotc.core.Contexts._
-import dotty.tools.dotc.reporting.Diagnostic
+import dotty.tools.dotc.reporting.{Diagnostic, StoreReporter}
 import dotty.tools.dotc.util.SourceFile
 
 import java.nio.file.Paths
@@ -16,25 +16,19 @@ object Scala3CompilerUtils {
 
     implicit val context: FreshContext = (new ContextBase).initialCtx.fresh.setReporter(reporter)
 
-    val fullClassPath =
-      builtinClasspaths.filterNot(_.isEmpty) ++
-        extraClasspath :+
-        context.settings.classpath.value
+    val fullClassPath = context.settings.classpath.value :: extraClasspath ::: builtinClasspaths.filterNot(_.isEmpty)
 
     context.setSetting(context.settings.classpath, fullClassPath.mkString(":"))
 
     val compiler = new Compiler()
     val run = compiler.newRun
-    run.compileSources(List(SourceFile.virtual("code.scala", code, maybeIncomplete = false)))
+    run.compileSources(SourceFile.virtual("scala_compiler_util_run_code.scala", code, maybeIncomplete = false) :: Nil)
 
     reporter.storedInfos
   }
 
-  private lazy val builtinClasspaths: Vector[String] =
-    Vector(
-      pathOf("scala.library.location"),
-      pathOf("scala.library2.location")
-    )
+  private lazy val builtinClasspaths: List[String] =
+    pathOf("scala.library.location") :: pathOf("scala.library2.location") :: Nil
 
   private def pathOf(jvmFlag: String) = {
     val jar = System.getProperty(jvmFlag)
@@ -44,8 +38,6 @@ object Scala3CompilerUtils {
 
   private lazy val baseDir = System.getProperty("user.dir")
 }
-
-import dotty.tools.dotc.reporting.StoreReporter
 
 class TestReporter extends StoreReporter(null) {
   def storedInfos: List[Diagnostic] = if (infos != null) infos.toList else List()
