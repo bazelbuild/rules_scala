71d7135808750623b2c9340704a23384de0caefd
Win Wang <1862202+wiwa@users.noreply.github.com>
2022-07-28 00:54:59 -0400

    Declare non-windows executable with .sh extension (#1407)
    * declare non-windows executable with .sh extension
    
    Co-authored-by: Shane Delmore <sdelmore@twitter.com>
    
    * Update ScalaBinaryRootpaths test
    
    * fixup! Update ScalaBinaryRootpaths test
    
    * Add regression test for runfile ArtifactPrefixConflictException
    
    Co-authored-by: Shane Delmore <sdelmore@twitter.com>
    
    * run lint
    
    Co-authored-by: Shane Delmore <sdelmore@twitter.com>

---
+++ test/artifact_prefix_conflict_example/example/ArtifactPrefixConflictExample.scala
@@ -0,0 +1,8 @@
+package example
+
+import example.Somefile
+
+object ArtifactPrefixConflictExample extends App {
+  println(s"Using Somefile: $Somefile")
+  println("In ArtifactPrefixConflictExample.scala")
+}
--- test/rootpaths_binary.sh
+++ test/rootpaths_binary.sh
@@ -3,9 +3,9 @@
 set -eou pipefail
 
 content="$(cat $1)"
-expected=$'test/ScalaBinary\ntest/ScalaBinary.jar'
+expected=$'test/ScalaBinary.jar\ntest/ScalaBinary.sh'
 if [ "$content" != "$expected" ]; then
     echo "Unexpected rootpaths: $content"
-    echo "$expected"
+    echo "Expected rootpaths: $expected"
     exit 1
 fi
