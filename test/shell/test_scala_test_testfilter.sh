fd28f69edf1f2df1889d5e1289e1e920550b6f3b
Stig Rohde Døssing <stig.doessing@crowdstrike.com>
2022-07-29 10:11:04 +0200

    comments

--- test/shell/test_scala_test_testfilter.sh
+++ test/shell/test_scala_test_testfilter.sh
@@ -5,11 +5,11 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 runner=$(get_test_runner "${1:-local}")
 
 test_scala_test_testfilter_class_selection() {
-  bazel test --test_output=errors --test_filter=A //test/scala_test_testfilter:tests
+  bazel test --test_output=errors --test_filter=A //test_expect_failure/scala_test_testfilter:tests
 }
 
 test_scala_test_testfilter_method_selection() {
-  bazel test --test_output=errors --test_filter=A --test_arg=-t --test_arg="test 1" //test/scala_test_testfilter:tests
+  bazel test --test_output=errors --test_filter=A --test_arg=-t --test_arg="test 1" //test_expect_failure/scala_test_testfilter:tests
 }
 
 $runner test_scala_test_testfilter_class_selection
