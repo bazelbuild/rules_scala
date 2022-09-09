a40063ef97688f056824b22b9e49fae6efd1df0f
Xùdōng Yáng <wyverald@gmail.com>
2022-09-09 10:37:37 +0200

    Fix test_scala_proto_library to accept @-prefixed labels (#1418)
    Needed to flip https://github.com/bazelbuild/bazel/issues/16196

--- test/shell/test_scala_proto_library.sh
+++ test/shell/test_scala_proto_library.sh
@@ -6,9 +6,9 @@ runner=$(get_test_runner "${1:-local}")
 
 test_scala_proto_library_action_label() {
   cd third_party/test/proto
-  local expected="action 'scala //:proto'"
+  local expected="action 'scala @{0,2}//:proto'"
   local action=$(bazel aquery --include_aspects 'mnemonic("Scalac", //...)' | grep ^action | head -n 1)
-  if [ "$action" != "$expected" ]; then
+  if [ "$action" != $expected ]; then
     echo "Expected \"$expected\" but got \"$action\""
     exit 1
   fi
