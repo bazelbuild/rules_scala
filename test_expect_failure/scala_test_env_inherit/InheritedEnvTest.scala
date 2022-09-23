f3741ea80edfe292e864726c0b884590af0a5b13
Vaidas Pilkauskas <vaidasp@wix.com>
2022-09-23 11:25:54 +0300

    Cleanup Bazel 5.2 test

--- test_expect_failure/scala_test_env_inherit/InheritedEnvTest.scala
+++ test_expect_failure/scala_test_env_inherit/InheritedEnvTest.scala
@@ -1,8 +1,8 @@
 import org.scalatest.funsuite._
 import org.scalatest.matchers.should._
 
-class A extends AnyFunSuite with Matchers {
-  test("number is positive") {
+class InheritedEnvTest extends AnyFunSuite with Matchers {
+  test("value from inherit_env") {
     sys.env("a") should equal("b")
   }
 }
