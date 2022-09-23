b69224cccb37bb31b9fb83772cabd684622fdc4f
Vaidas Pilkauskas <vaidasp@wix.com>
2022-09-23 14:06:15 +0300

    Cleanup Bazel 5.2 test (#1426)
    * Cleanup Bazel 5.2 test

--- test/shell/test_inherited_environment.sh
+++ test/shell/test_inherited_environment.sh
@@ -5,7 +5,7 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 runner=$(get_test_runner "${1:-local}")
 
 function test_inherited_environment() {
-  a=b bazel test //test_bazel_5_2/inherited_environment:a
+  a=b bazel test //test_expect_failure/scala_test_env_inherit:inherit_a
 }
 
 $runner test_inherited_environment
