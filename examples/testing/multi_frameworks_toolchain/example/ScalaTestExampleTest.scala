330081d031c4a81b6309d58a243c2ba638bbd175
Kevin Boyette <KevinBoyette@users.noreply.github.com>
2024-03-22 10:38:02 -0400

    fix typo in example tests (Exmaple => Example) (#1553)

--- examples/testing/multi_frameworks_toolchain/example/ScalaTestExampleTest.scala
+++ examples/testing/multi_frameworks_toolchain/example/ScalaTestExampleTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 
 class ScalaTestExampleTest extends AnyFlatSpec with Matchers {
-  "Exmaple" should "pass" in {
+  "Example" should "pass" in {
     1 must be(1)
   }
 }
