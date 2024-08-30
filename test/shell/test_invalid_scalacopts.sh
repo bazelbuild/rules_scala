d4f20b98741641fd51143e2edc5d213d60794313
Wojciech Mazur <wmazur@virtuslab.com>
2024-08-30 21:08:52 +0200

    Addi tests to ensure corect error message and no stack traces are shown

---
+++ test/shell/test_invalid_scalacopts.sh
@@ -0,0 +1,43 @@
+# shellcheck source=./test_runner.sh
+
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+. "${dir}"/test_runner.sh
+. "${dir}"/test_helper.sh
+runner=$(get_test_runner "${1:-local}")
+
+test_logs_contains() {
+  scalaVersion=$1
+  expected=$2
+  
+  bazel build \
+   --repo_env=SCALA_VERSION=${scalaVersion} \
+   //test_expect_failure/scalacopts_invalid:empty \
+   2>&1 | grep "$expected"
+}
+
+test_logs_not_contains() {
+  scalaVersion=$1
+  expected=$2
+
+  bazel build \
+   --repo_env=SCALA_VERSION=${scalaVersion} \
+   //test_expect_failure/scalacopts_invalid:empty \
+   2>&1 | grep -v "$expected"
+}
+
+test_logs_contains_no_stacktrace() {
+  bazel build \
+   --extra_toolchains="//test_expect_failure/scalacopts_invalid" \
+   --repo_env=SCALA_VERSION=${scalaVersion} \
+   //test_expect_failure/scalacopts_invalid:empty
+}
+
+for scalaVersion in 2.12.19 2.13.14 3.3.3; do
+  if [[ "$scalaVersion" == 3.* ]]; then
+    $runner test_logs_contains $scalaVersion "not-existing is not a valid choice for -source"
+  else
+    $runner test_logs_contains $scalaVersion "bad option: '-source:not-existing'"
+  fi
+  $runner test_logs_contains $scalaVersion 'Failed to invoke Scala compiler, ensure passed options are valid'
+  $runner test_logs_not_contains $scalaVersion 'at io.bazel.rulesscala.scalac.ScalacWorker.main'
+done
--- test_rules_scala.sh
+++ test_rules_scala.sh
@@ -58,3 +58,4 @@ $runner bazel build //test_statsfile:SimpleNoStatsFile_statsfile --extra_toolcha
 . "${test_dir}"/test_persistent_worker.sh
 . "${test_dir}"/test_semanticdb.sh
 . "${test_dir}"/test_scaladoc.sh
+. "${test_dir}"/test_invalid_scalacopts.sh
