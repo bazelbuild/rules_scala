91a45f791a0f05590dcb983bba1b7b38499192fe
Wojciech Mazur <wmazur@virtuslab.com>
2024-10-24 09:04:19 +0200

    Collect diagnostics in Scala 3 (#1611)
    * Add ProtoReporter and DepsTrackingReporter for Scala3. Collect diagnostics in Scala 3
    
    * Lint + rename ConsoleReporter -> BazelConsoleReporter to disambigiuate with dotc.reporting.ConsoleReporter
    
    * Revert spuriously commented out tests in `test_version.sh`

--- test/diagnostics_reporter/DiagnosticsReporterTest.java
+++ test/diagnostics_reporter/DiagnosticsReporterTest.java
@@ -7,54 +7,14 @@ import java.util.List;
 import java.util.Map;
 
 public class DiagnosticsReporterTest {
-  @SuppressWarnings("DoubleBraceInitialization")
-  private static final Map<String, diagnostics_reporter.VerifyDiagnosticsOutput[]> tests =
-      new HashMap<String, diagnostics_reporter.VerifyDiagnosticsOutput[]>() {
-        {
-          put(
-              "error_file",
-              new diagnostics_reporter.VerifyDiagnosticsOutput[] {
-                new diagnostics_reporter.VerifyDiagnosticsOutput(
-                    Diagnostics.Severity.ERROR, "\')\' expected but \'}\' found.")
-              });
-          put(
-              "two_errors_file",
-              new diagnostics_reporter.VerifyDiagnosticsOutput[] {
-                new diagnostics_reporter.VerifyDiagnosticsOutput(
-                    Diagnostics.Severity.ERROR, "not found: value printn"),
-                new diagnostics_reporter.VerifyDiagnosticsOutput(
-                    Diagnostics.Severity.ERROR, "not found: value prinf")
-              });
-          put(
-              "warning_file",
-              new diagnostics_reporter.VerifyDiagnosticsOutput[] {
-                new diagnostics_reporter.VerifyDiagnosticsOutput(
-                    Diagnostics.Severity.WARNING, "Unused import")
-              });
-          put(
-              "error_and_warning_file",
-              new diagnostics_reporter.VerifyDiagnosticsOutput[] {
-                new diagnostics_reporter.VerifyDiagnosticsOutput(
-                    Diagnostics.Severity.WARNING, "Unused import"),
-                new diagnostics_reporter.VerifyDiagnosticsOutput(
-                    Diagnostics.Severity.ERROR, "not found: value printn")
-              });
-          put(
-              "info_file",
-              new diagnostics_reporter.VerifyDiagnosticsOutput[] {
-                new diagnostics_reporter.VerifyDiagnosticsOutput(
-                    Diagnostics.Severity.INFORMATION, "[running phase parser on InfoFile.scala]")
-              });
-        }
-      };
-
-  public static void main(String[] args) throws IOException {
+   public static void main(String[] args) throws IOException {
     if (args.length != 1) throw new IllegalArgumentException("Args: <diagnostics_output>");
 
     String diagnosticsOutput = args[0];
-    for (Map.Entry<String, VerifyDiagnosticsOutput[]> entry : tests.entrySet()) {
+    for (Map.Entry<String, VerifyDiagnosticsOutput[]> entry : DiagnosticsReporterTestCases.tests.entrySet()) {
       String test = entry.getKey();
       VerifyDiagnosticsOutput[] expectedDiagnosticsOutputs = entry.getValue();
+      System.out.println("Test case: " + test);
       List<Diagnostics.Diagnostic> diagnostics =
           VerifyDiagnosticsOutput.getDiagnostics(
               diagnosticsOutput + "/" + test + ".diagnosticsproto");
