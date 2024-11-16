84e4a74e6668b0a43186ca8db6243b82e08fb81c
crt-31 <cthomsen@plexsys.com>
2024-11-15 17:07:19 -0800

    Fixed tests to pass on windows when autocrlf=false

--- test/src/main/scala/scalarules/test/resources/ScalaLibResourcesFromExternalScalaTest.scala
+++ test/src/main/scala/scalarules/test/resources/ScalaLibResourcesFromExternalScalaTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.funsuite._
 class ScalaLibResourcesFromExternalScalaTest extends AnyFunSuite {
 
   test("Scala library depending on resources from external resource-only jar should allow to load resources") {
-    val expectedString = String.format("A resource%n"); //Using platform dependent newline (%n)
+    val expectedString = String.format("A resource\n");
     assert(get("/resource.txt") === expectedString)
   }
 
