9f0f5baf45aef020961826bdcec4405535ccd14b
Wojciech Mazur <wmazur@virtuslab.com>
2025-06-10 08:33:27 +0200

    Upgrade Scala versions 3.7.0 -> 3.7.1 (#1746)

--- test_thirdparty_version.sh
+++ test_thirdparty_version.sh
@@ -15,7 +15,7 @@ runner=$(get_test_runner "${1:-local}")
 
 
 # Latest version of each major version
-$runner test_scala_version "3.7.0" # Latest Next version
+$runner test_scala_version "3.7.1" # Latest Next version
 $runner test_scala_version "3.3.6" # Latest LTS version
 $runner test_scala_version "3.1.3" # First supported major for Scala 3, max supported JDK=18
 $runner test_scala_version "2.13.16"
